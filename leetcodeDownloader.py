0f7b2d2325bf0c6118c99325cc7b4fdae7fbfd1a
Ning Tang <tecton69@gmail.com>
2014-11-26 00:21:39 +0800

    Add two source files.

---
+++ leetcodeDownloader.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import leetcodeHTTPHelper
+import re
+import json
+from bs4 import BeautifulSoup
+import os
+import sys
+
+def downloadSubmission(session, name, URL):
+    '''
+    parse code from source and save it to file.
+    '''
+    filename = 'code/' + name + '.cpp'
+    if os.path.isfile(filename):
+        return
+    r = session.get(URL)
+    code = re.search(r'scope\.code\.cpp\ =\ \'(.*)', r.content).group(1)[:-2] #discard last two characters
+    code = code.encode('utf8')
+    s = json.loads('{"code": "%s"}' % code)
+    print 'add file: ' + filename
+    f = open(filename, 'w')
+    f.write(s['code'])
+    f.close()
+
+def checkSubmissions(username, password):
+    SUBMISSION_URL = 'https://oj.leetcode.com/submissions/'
+    BASE_URL = 'https://oj.leetcode.com'
+    s = leetcodeHTTPHelper.getSubmission(username, password)
+    if not os.path.exists('code'):
+        os.makedirs('code')
+
+    for i in range(1, 10000):
+        print 'Check page ' + str(i)
+        r = s.get(SUBMISSION_URL + str(i))
+        soup = BeautifulSoup(r.text)
+        try:
+            # delete '\n' between tr tags
+            trList = filter(lambda a: a != '\n', list(soup.tbody.children))
+        except:
+            print 'All submissions checked.'
+            break
+
+        # structure of html tags are hard coded
+        for tr in trList:
+            tr = filter(lambda a: a != '\n', list(tr.children))
+            submissionStatus = tr[2]
+            try:
+                result = submissionStatus.a.strong.contents[0]
+                if (result == 'Accepted'):
+                    problemName = tr[1].a.contents[0]
+                    codeURL = BASE_URL + submissionStatus.a['href']
+                    downloadSubmission(s, problemName, codeURL)
+            except:
+                pass
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 3):
+        print "usage: leetcodeDownloader.py username password"
+    else:
+        checkSubmissions(sys.argv[1], sys.argv[2])
+
---
+++ leetcodeHTTPHelper.py
@@ -0,0 +1,19 @@
+import requests
+
+URL = 'https://oj.leetcode.com/accounts/login/'
+
+s = requests.Session()
+s.get(URL)
+csrftoken = s.cookies['csrftoken']
+
+headers = {
+    'User-Agent': 'Mozilla/5.0',
+    'Referer': URL
+}
+           
+def getSubmission(username, password):
+    payload = {'csrfmiddlewaretoken': csrftoken,
+               'login': username,
+               'password': password}
+    s.post(URL, data=payload, headers=headers)
+    return s
