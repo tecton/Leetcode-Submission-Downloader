104f3e63c746b098241387def04684034ee23b53
Xiangyu Liu <xliu215@uic.edu>
2017-04-08 16:25:09 -0700

    adapt to new version of leetcode 2017.4

--- leetcodeHTTPHelper.py
+++ leetcodeHTTPHelper.py
@@ -1,19 +1,20 @@
 import requests
 
-URL = 'https://leetcode.com/accounts/login/'
-
-s = requests.Session()
-s.get(URL)
-csrftoken = s.cookies['csrftoken']
-
-headers = {
-    'User-Agent': 'Mozilla/5.0',
-    'Referer': URL
-}
+USER_AGENT = 'Mozilla/5.0'
+LOGIN_URL = 'https://leetcode.com/accounts/login/'
            
-def getSubmission(username, password):
+def login(username, password):
+    headers = {
+        'User-Agent': USER_AGENT,
+        'Referer': LOGIN_URL
+    }
+
+    s = requests.Session()
+    s.get(LOGIN_URL)
+    csrftoken = s.cookies['csrftoken']
+
     payload = {'csrfmiddlewaretoken': csrftoken,
                'login': username,
                'password': password}
-    s.post(URL, data=payload, headers=headers)
+    s.post(LOGIN_URL, data=payload, headers=headers)
     return s
